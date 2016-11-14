c3ba6a3a5d01bb6857717dca92d016957a77d9ca
UBB <ubb@example.com>
2016-11-15 08:53:08 +0900

    apply url change

--- utils/script.sh
+++ utils/script.sh
@@ -1,41 +1,46 @@
-﻿#!/bin/sh
+﻿#!/bin/bash
 
 ##argument check
 if [ 1 -ne $# ]
 then
 	echo "arg error"
 	exit 1
+else
+	file=$1
 fi
-file=$1
 
 ##Backup
-cp $file $file.org
+cp "$file" "$file.orig"
 
-##헤더(목차) 날리기(txt가 유니코드UTF-8로 저장되어야 함)
-function cut_header(){
-	sed -e '1,/^용어사전/d' ./korean.txt > temp.txt
-	mv -f temp.txt $file
+##헤더(목차)/테일 날리기(txt가 유니코드UTF-8로 저장되어야 함)
+function trim(){
+	sed -e '1,/^용어사전/d' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	echo "Delete Header."
+	
+	sed -e '/^용어사전/,$d' "$file" > temp.txt
+	mv -f temp.txt "$file"
+	echo "Delete Tail."
 }
 
 ## 문법 적용
 function markdown_change(){
 	##대문단 md 문법 적용
-	sed -e 's/\(^[0-9]\{3\}\. \)/<br>\n\n#### \1/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/\(^[0-9]\{3\}\. \)/<br>\n\n#### \1/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	
 	##중, 소문단 md 문법 적용
-	sed -e 's/\(^[0-9]\{3\}\.[0-9]\+[.|a-z]*\)/**\1**/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/\(^[0-9]\{3\}\.[0-9]\+[.|a-z]*\)/**\1**/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	
 	##소문단 md 문법 적용
-	sed -e 's/\(^\*\*[0-9]\{3\}\.[0-9]\+[a-z]\+\*\* .\+$\)/<p class="clause" markdown="1">\1<\/p>/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/\(^\*\*[0-9]\{3\}\.[0-9]\+[a-z]\+\*\* .\+$\)/<p class="clause" markdown="1">\1<\/p>/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	#--예외 : 205.3i-j, 509.1b, 810.7b
 	
 	##example 서식 적용
-	sed -e 's/\(^Example:\) \(.\+$\)/<p class="example" markdown="1"> **\1** \2<\/p>/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/\(^Example:\) \(.\+$\)/<p class="example" markdown="1"> **\1** \2<\/p>/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	#--예외 : 607.5.
 	echo "Tag apply complete."
 }
@@ -43,17 +48,20 @@ function markdown_change(){
 function symbol_change(){
 	##심볼변환
 	#한단어 : 
-	sed -e 's/{\([T|Q|C|W|B|U|R|G|E|S|P|X]\|PW\|CHAOS\|[0-9]\{1,2\}\)}/![symbol\1](\/assets\/symbols\/\1.gif)/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/{\([T|Q|C|W|B|U|R|G|E|S|P|X]\|PW\|CHAOS\|[0-9]\{1,2\}\)}/![symbol\1](\/assets\/symbols\/\1.gif)/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	#하이브리드:
-	sed -e 's/{\([WUBRG2]\)\/\([WUBRGP]\)}/![symbol\1\2](\/assets\/symbols\/\1\2.gif)/g' $file > temp.txt
-	mv -f temp.txt $file
+	sed -e 's/{\([WUBRG2]\)\/\([WUBRGP]\)}/![symbol\1\2](\/assets\/symbols\/\1\2.gif)/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
 	echo "Symbol change complete."
 }
 
 ##홈페이지 링크
-
-#echo "Hyperlink change complete."
+function url_change() {
+	sed -e 's/\(https\?\:\/\/\)\?\([\da-zA-Z]\+\)\.\([A-Za-z\.]\{2,6\}\)\([\/A-Za-z0-9_\.-]*\)*\([A-Za-z0-9]\)\/\?/[\1\2.\3\4\5](http:\/\/\2.\3\4\5)/g' "$file" > temp.txt
+	mv -f temp.txt "$file"
+	echo "URL change complete."
+}
 
 ##주석변환
 
@@ -64,9 +72,10 @@ function symbol_change(){
 ##파일앞에 헤더 달기
 
 ##START
-cut_header
+trim
 markdown_change
 symbol_change
+url_change
 
 echo "Success!"
 exit 0
